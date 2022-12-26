2be2a42ee8da628f7427ef8cff87602deb6d0892
도넛맨 <hp196@naver.com>
2022-12-26 19:10:20 +0900

    Item 도메인 비즈니스 로직 추가

--- src/main/java/jpa_prt/domain/item/Item.java
+++ src/main/java/jpa_prt/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpa_prt.domain.item;
 
 import jpa_prt.domain.Category;
+import jpa_prt.exception.notEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -26,4 +27,24 @@ public abstract class Item {
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
 
+
+    //비즈니스 로직
+
+    /**
+     * 수량 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * 수량 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity -= quantity;
+        if (restStock < 0) {
+            throw new notEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
---
+++ src/main/java/jpa_prt/exception/notEnoughStockException.java
@@ -0,0 +1,24 @@
+package jpa_prt.exception;
+
+public class notEnoughStockException extends RuntimeException {
+
+    public notEnoughStockException() {
+        super();
+    }
+
+    public notEnoughStockException(String message) {
+        super(message);
+    }
+
+    public notEnoughStockException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public notEnoughStockException(Throwable cause) {
+        super(cause);
+    }
+
+    protected notEnoughStockException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
